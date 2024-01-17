337b688719242227e4810b7badd506e28238c801
LinTech <hernandezmarkjulius24@gmail.com>
2024-01-17 13:42:51 +0800

    Adding history

--- app.component.ts
+++ app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent {
 
 	EqualOnClick(): void {
 		const equal = this.Calculator.equalResult();
+		console.log(equal);
 	}
 
 	percentOnClick(): void {
--- calculator/calculator.module.ts
+++ calculator/calculator.module.ts
@@ -3,6 +3,11 @@ import { CommonModule } from "@angular/common";
 import * as math from "mathjs";
 // import { last } from "rxjs";
 
+interface history {
+	historyOperation: any;
+	historyAnswer: any;
+}
+
 @NgModule({
 	declarations: [],
 	imports: [CommonModule],
@@ -11,6 +16,7 @@ export class CalculatorModule {
 	private _textBoxValue = "";
 	private _equal: number = 0;
 	private percentNumber: number = 0.01;
+	public historyArr: history[] = [];
 
 	// get the value for text box
 	get textBoxValue(): string {
@@ -26,23 +32,28 @@ export class CalculatorModule {
 	}
 
 	computePercent(): void {
-		const split = this._textBoxValue.split(/[-+*/]/).filter(Boolean);
+		const split = this._textBoxValue.split(/([-+*/])/).filter(Boolean);
 
-		const lastNumber: string = split.length ? split[split.length - 1] : "0";
-
-		const convertToNum = parseFloat(lastNumber);
-		this._equal = convertToNum * 0.01;
-		const convertTotalToString: string = this._equal.toString();
-		this._textBoxValue = convertTotalToString;
+		if (split.length >= 1) {
+			const lastNumber: any = split[split.length - 1];
+			const convertToPercent: any = parseFloat(lastNumber) * this.percentNumber;
+			split[split.length - 1] = convertToPercent.toString();
+			this._textBoxValue = split.join("");
+		}
 	}
 
-	equalResult(): void {
+	equalResult(): any {
 		const expression = this._textBoxValue.trim();
 
 		if (expression) {
 			this._equal = math.evaluate(expression);
 		}
 		const convertTotalToString: string = this._equal.toString();
+		this.historyArr.push({
+			historyOperation: this._textBoxValue,
+			historyAnswer: convertTotalToString,
+		});
 		this._textBoxValue = convertTotalToString;
+		return this.historyArr;
 	}
 }
